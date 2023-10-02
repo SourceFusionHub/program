349ac07fc7d9e1d671cb6284a3f39828199c7427
Pritish Bhawal <pritishyo10@gmail.com>
2023-10-03 03:09:59 +0530

    added areaofparallelogram

---
+++ YNCEg8/TLYg84.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+double areaofparallelogram(double base,double height){
+    return base*height;
+}
+int main(){
+    double res=areaofparallelogram(2.4,5);
+    cout<<res;
+}
