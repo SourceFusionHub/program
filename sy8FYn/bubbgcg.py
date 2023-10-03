363bfaf690617d414dd97fd9afd5493dce5785fb
bhargavshirin <bnalamat@gitam.in>
2023-10-03 19:11:59 +0530

    added bubble sort

---
+++ sy8FYn/bubbgcg.py
@@ -0,0 +1,15 @@
+def bubble_sort(arr):
+    n = len(arr)
+  
+    for i in range(n):
+    
+        for j in range(0, n-i-1):
+     
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+    print("Unsorted array:", arr)
+    bubble_sort(arr)
+    print("Sorted array:", arr)
