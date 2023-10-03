1db484ab05e5f29116443455f23ddd989ad8d6fc
Ashwani kumar pandey <99007418+ashwani798@users.noreply.github.com>
2023-10-03 17:25:12 +0530

    Create ZpQ0RY.rb
    Signed-off-by: Ashwani kumar pandey  <99007418+ashwani798@users.noreply.github.com>

---
+++ rn4rBK/ZpQ0RY.rb
@@ -0,0 +1,9 @@
+def calculate_perimeter(length, width)
+  perimeter = 2 * (length + width)
+  return perimeter
+end
+
+# Example usage
+length = 5
+width = 3
+puts "Perimeter of the rectangle: #{calculate_perimeter(length, width)}"
