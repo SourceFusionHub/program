2f3b95295e957fc20fa35a307852b7ec9e1398ac
Yash1038 <yashaswilnpasumarthy@gmail.com>
2023-10-03 12:44:48 +0530

    Removed redundant lines

--- IrW9fz/9tC6xx.cpp
+++ IrW9fz/9tC6xx.cpp
@@ -1,16 +1,5 @@
 #include<bits/stdc++.h>
 
-#define ll long long int
-#define ld long double
-#define ff(i,a,b) for(int i=a;i<b;i++)
-#define fr(i,a,b) for(int i=a;i>=b;i--)
-#define vi vector<int>
-#define vll vector<ll>
-#define pii pair<int,int>
-#define pll pair<ll,ll>
-#define pb push_back
-#define mp make_pair
-
 using namespace std; 
 
 // Implementing a function that returns the sum of 1 + 1/2 + 1/3 + ... + 1/n
