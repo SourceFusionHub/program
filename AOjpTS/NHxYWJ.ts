8286eec784a77eee84c1cb507f44607c8980d584
Sai Prasana Panigrahi <125462900+saai07@users.noreply.github.com>
2023-10-03 14:53:19 +0530

    Create NHxYWJ.ts
    Signed-off-by: Sai Prasana Panigrahi <125462900+saai07@users.noreply.github.com>

---
+++ AOjpTS/NHxYWJ.ts
@@ -0,0 +1,12 @@
+function printFirstTenEvenNumbers() {
+    let count = 0;
+    let number = 2;
+
+    while (count < 10) {
+        console.log(number);
+        number += 2;
+        count++;
+    }
+}
+
+printFirstTenEvenNumbers();
