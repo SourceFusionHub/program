6bc9134005d085b165b81d1e8d0a8fffe3f397b7
Shakthi Warnakulasuriya <115597935+ShakthiW@users.noreply.github.com>
2023-10-04 09:14:21 +0530

    Create Y9hz3v.swift
    Signed-off-by: Shakthi Warnakulasuriya <115597935+ShakthiW@users.noreply.github.com>

---
+++ QX8qjk/Y9hz3v.swift
@@ -0,0 +1,14 @@
+// Define a function to calculate the product of two numbers
+func calculateProduct(num1: Int, num2: Int) -> Int {
+    return num1 * num2  // Return the product of num1 and num2
+}
+
+// Input numbers
+let number1 = 10
+let number2 = 5
+
+// Call the function to calculate the product
+let product = calculateProduct(num1: number1, num2: number2)
+
+// Print the result
+print("The product of \(number1) and \(number2) is: \(product)")
