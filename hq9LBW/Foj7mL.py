cecc8b62058bfc8af696dfa441494d8724927639
Aditya Pathak <68177924+Phoenix1504e@users.noreply.github.com>
2023-10-05 04:39:11 +0530

    Create Foj7mL.py
    Signed-off-by: Aditya Pathak <68177924+Phoenix1504e@users.noreply.github.com>

---
+++ hq9LBW/Foj7mL.py
@@ -0,0 +1,4 @@
+#A program to convert temperature from celcius to fahreheit in python
+celsius = float(input("Enter temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit")
