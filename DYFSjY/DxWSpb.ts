a616c2b2ab6aa671a95e4c2f77fbefebeaa6b06d
Shakthi Warnakulasuriya <115597935+ShakthiW@users.noreply.github.com>
2023-10-04 08:58:21 +0530

    Create DxWSpb.ts
    Signed-off-by: Shakthi Warnakulasuriya <115597935+ShakthiW@users.noreply.github.com>

---
+++ DYFSjY/DxWSpb.ts
@@ -0,0 +1,12 @@
+function calculateCubeArea(sideLength: number): number {
+  return 6 * sideLength ** 2;
+}
+
+
+// example usage
+const sideLength = 5;
+const cubeArea = calculateCubeArea(sideLength);
+
+console.log(`The surface area of the cube is ${cubeArea} square units.`);
+
+// output - The surface area of the cube is 150 square units
