5bd1837724354f67201c3ad8566417bd58189b5a
Nirbhay Tiwari <143536386+Nirbhay-279@users.noreply.github.com>
2023-10-04 15:49:19 +0530

    Create 31z6rB.py
    Signed-off-by: Nirbhay Tiwari <143536386+Nirbhay-279@users.noreply.github.com>

---
+++ 6itv6k/31z6rB.py
@@ -0,0 +1,18 @@
+# Initialize a variable to start counting from 1
+count = 1
+
+# Use a while loop to print numbers from 1 to 10
+while count <= 10:
+    print(count)
+    count += 1  # Increment the count variable
+
+# Output: 1
+#         2
+#         3
+#         4
+#         5
+#         6
+#         7
+#         8
+#         9
+#         10
