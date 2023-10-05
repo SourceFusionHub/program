49a03b304c21e570392aab97941f1b81e3a3a2e3
RedStar6 <richmondnkrumah04@gmail.com>
2023-10-05 20:57:06 +0000

    Create JNnygT.java

---
+++ 9k0VGh/JNnygT.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SquarePerimeterCalculator {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the side length of the square
+        System.out.print("Enter the side length of the square: ");
+
+        // Read the side length from the user
+        double sideLength = scanner.nextDouble();
+
+        // Close the Scanner to prevent resource leak
+        scanner.close();
+
+        // Calculate the perimeter of the square
+        double perimeter = 4 * sideLength;
+
+        // Print the result
+        System.out.println("The perimeter of the square is: " + perimeter);
+    }
+}
