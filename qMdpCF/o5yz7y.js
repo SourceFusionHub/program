6ee16a842d5d44b9b8068364ced110ebd4a6876f
Aditya Aryaman Das <aditya.aryamandas.mec21@itbhu.ac.in>
2023-10-05 17:58:13 +0530

    Added Comments

--- qMdpCF/o5yz7y.js
+++ qMdpCF/o5yz7y.js
@@ -14,3 +14,12 @@ if (!isNaN(num1) && !isNaN(num2) && !isNaN(num3)) {
   console.log("Invalid input. Please enter valid numbers.");
 }
 
+// In this code:
+
+// We use the prompt function to display a dialog box to the user and get their input for three numbers. We use parseFloat to convert the input strings into floating-point numbers.
+
+// We check if the input is valid numbers using isNaN (Is Not a Number) function. If all three inputs are valid numbers, we proceed to calculate the product.
+
+// If the inputs are valid, we calculate the product of the three numbers using the * operator.
+
+// Finally, we display the calculated product in the console. If the user enters invalid input (e.g., non-numeric values), it will display "Invalid input. Please enter valid numbers."
