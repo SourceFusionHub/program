37158c4d6927577a7d88359a02768c1ea175db17
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2023-10-05 19:13:27 +0530

    Update I2DVpQ.dart
    Signed-off-by: Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>

--- JaFfnJ/I2DVpQ.dart
+++ JaFfnJ/I2DVpQ.dart
@@ -1,36 +1,20 @@
-// C++ program to count the number of occurrence
-// of a word in the given string
-#include <bits/stdc++.h>
-using namespace std;
-
-int countOccurrences(string str, string word)
-{
-	// Split the string into words
-	stringstream ss(str);
-	string token;
-	vector<string> wordslist;
-
-	while (ss >> token) {
-		wordslist.push_back(token);
-	}
-
-	// Count the number of occurrences of the given word
-	int count = 0;
-	for (int i = 0; i < wordslist.size(); i++) {
-		if (wordslist[i] == word) {
-			count++;
-		}
-	}
-
-	return count;
+int countOccurrences(String sentence, String word) {
+  final wordsList = sentence.split(RegExp(r'\s+')); // Split by spaces
+  int count = 0;
+  
+  for (String w in wordsList) {
+    if (w == word) {
+      count++;
+    }
+  }
+  
+  return count;
 }
 
-// Driver code
-int main()
-{
-	string str = "457ayan "
-				"for ayan";
-	string word = "protal ayan";
-	cout << countOccurrences(str, word);
-	return 0;
+void main() {
+  String sentence = "457ayan for ayan";
+  String word = "ayan";
+  int occurrences = countOccurrences(sentence, word);
+  
+  print("The word '$word' occurs $occurrences times in the sentence.");
 }
