e81f275f2b2ee691ad880268503f66899813af0e
Aditya Aryaman Das <128703909+alienishi@users.noreply.github.com>
2023-10-05 17:46:21 +0530

    Update o5yz7y.js
    Signed-off-by: Aditya Aryaman Das <128703909+alienishi@users.noreply.github.com>

--- qMdpCF/o5yz7y.js
+++ qMdpCF/o5yz7y.js
@@ -1,1 +1,16 @@
+// Get input from the user for three numbers
+const num1 = parseFloat(prompt("Enter the first number:"));
+const num2 = parseFloat(prompt("Enter the second number:"));
+const num3 = parseFloat(prompt("Enter the third number:"));
+
+// Check if the input is valid numbers
+if (!isNaN(num1) && !isNaN(num2) && !isNaN(num3)) {
+  // Calculate the product of the three numbers
+  const product = num1 * num2 * num3;
+
+  // Display the result
+  console.log(`The product of ${num1}, ${num2}, and ${num3} is: ${product}`);
+} else {
+  console.log("Invalid input. Please enter valid numbers.");
+}
 
