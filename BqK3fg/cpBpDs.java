78201c153ba41b00cf6d103ce0bbbe67ed70f069
shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>
2023-10-06 00:32:16 +0530

    Update cpBpDs.java(Modification)
    Signed-off-by: shuddhi-jain <92723741+shuddhi-jain@users.noreply.github.com>

--- BqK3fg/cpBpDs.java
+++ BqK3fg/cpBpDs.java
@@ -1,37 +1,39 @@
 import java.io.File;
 import java.io.FilePermission;
 import java.security.Permission;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CustomSecurityManager extends SecurityManager {
 
+    private static final Logger LOGGER = Logger.getLogger(CustomSecurityManager.class.getName());
+
     @Override
     public void checkPermission(Permission perm) {
-        // Check if the permission is a FilePermission
         if (perm instanceof FilePermission) {
             String actions = perm.getActions();
-            if (actions.contains("read")) {
-                // Check if the file read action is allowed
-                if (!isFileAllowed(((FilePermission) perm).getName(), "read")) {
-                    throw new SecurityException("File read access not allowed: " + perm.getName());
-                }
-            } else if (actions.contains("write")) {
-                // Check if the file write action is allowed
-                if (!isFileAllowed(((FilePermission) perm).getName(), "write")) {
-                    throw new SecurityException("File write access not allowed: " + perm.getName());
-                }
+            String filePath = ((FilePermission) perm).getName();
+            if (actions.contains("read") && !isFileAllowed(filePath, "read")) {
+                logAccessDenied("File read access not allowed: " + filePath);
+                throw new SecurityException("File read access not allowed: " + filePath);
+            } else if (actions.contains("write") && !isFileAllowed(filePath, "write")) {
+                logAccessDenied("File write access not allowed: " + filePath);
+                throw new SecurityException("File write access not allowed: " + filePath);
             }
         }
     }
 
     private boolean isFileAllowed(String filename, String action) {
-        
         return filename.startsWith("/tmp/") || filename.startsWith("C:/temp/");
+    }
+
+    private void logAccessDenied(String message) {
+        LOGGER.log(Level.WARNING, message);
     }
 
     public static void main(String[] args) {
         System.setSecurityManager(new CustomSecurityManager());
 
-        
         try {
             File file = new File("/home/user/sensitive.txt");
             if (file.exists()) {
