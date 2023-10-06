f0cf403061f40eaa2781615e29bff5d4bac1fff8
Jethendra-17 <146614641+Jethendra-17@users.noreply.github.com>
2023-10-06 22:52:51 +0530

    Create dq4BBz.swift
    Signed-off-by: Jethendra-17 <146614641+Jethendra-17@users.noreply.github.com>

---
+++ IwahEz/dq4BBz.swift
@@ -0,0 +1,10 @@
+// Function to calculate area of rectangle
+func calculateArea(length: Int, width: Int) -> Int {
+    return length * width
+}
+
+// Example usage
+let length = 5
+let width = 10
+let area = calculateArea(length: length, width: width)
+print("The area of the rectangle is \(area)")
