87efa3474208ef91a9c2efc6bbda497a525faffa
Jethendra-17 <146614641+Jethendra-17@users.noreply.github.com>
2023-10-06 22:30:58 +0530

    Create KuYswI.java
    Signed-off-by: Jethendra-17 <146614641+Jethendra-17@users.noreply.github.com>

---
+++ JbS997/KuYswI.java
@@ -0,0 +1,39 @@
+public class QuickSort {
+
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivot = partition(array, low, high);
+            quickSort(array, low, pivot - 1);
+            quickSort(array, pivot + 1, high);
+        }
+    }
+
+    private static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 3, 2, 1, 4};
+        quickSort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
