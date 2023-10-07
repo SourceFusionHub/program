cfe2359a1359769826cac0cad88a7e7d403b104f
Akash Pambhar <akash.pambhar@yahoo.com>
2023-10-07 13:32:31 +0530

    Create 9umQgW.java

---
+++ vVWNdT/9umQgW.java
@@ -0,0 +1,10 @@
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String str1 = "Hello, ";
+        String str2 = "world!";
+
+        String result = str1.concat(str2);
+
+        System.out.println(result);
+    }
+}
