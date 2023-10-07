62db95801078e0f03e6af296cbe1fc731278a60c
Sarnendu Paul <78204343+Sarnendu-Paul@users.noreply.github.com>
2023-10-07 13:05:13 +0530

    Create jgd3Pi.py
    Signed-off-by: Sarnendu Paul <78204343+Sarnendu-Paul@users.noreply.github.com>

---
+++ YtGL1S/jgd3Pi.py
@@ -0,0 +1,7 @@
+order_amt = 123.4521879
+print('\nThe total order amount comes to %f' % order_amt)
+print('The total order amount comes to %.1f' % order_amt)
+print('The total order amount comes to %.2f' % order_amt)
+print('The total order amount comes to %.3f' % order_amt)
+print('The total order amount comes to %.4f' % order_amt)
+print()
